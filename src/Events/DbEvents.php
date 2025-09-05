351e0d314a589f13197c2ee4f059db0e7ac2ac23
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 07:45:34 +0500

    Refactor: merge `ConnectionEvent` functionality into `DbEvents` and remove `ConnectionEvent` class.

--- src/Events/DbEvents.php
+++ src/Events/DbEvents.php
@@ -9,17 +9,48 @@ declare(strict_types=1);
 namespace Charcoal\Database\Events;
 
 use Charcoal\Database\DatabaseClient;
+use Charcoal\Database\Events\Connection\ConnectionError;
+use Charcoal\Database\Events\Connection\ConnectionStateContext;
+use Charcoal\Database\Events\Connection\ConnectionSuccess;
+use Charcoal\Database\Events\Connection\ConnectionWaiting;
+use Charcoal\Events\BehaviorEvent;
+use Charcoal\Events\Dispatch\DispatchReport;
+use Charcoal\Events\Subscriptions\Subscription;
 
 /**
- * Class DbEvents
- * @package Charcoal\Database\Events
+ * Represents a database-related event management system that extends base behavior events.
+ * This class facilitates subscribing to database connection events and dispatching state updates.
  */
-readonly class DbEvents
+class DbEvents extends BehaviorEvent
 {
-    public ConnectionEvent $connectionState;
-
-    public function __construct(DatabaseClient $db)
+    /**
+     * @param DatabaseClient $client
+     */
+    public function __construct(protected readonly DatabaseClient $client)
     {
-        $this->connectionState = new ConnectionEvent($db);
+        parent::__construct("onConnection", [
+            ConnectionStateContext::class,
+            ConnectionWaiting::class,
+            ConnectionSuccess::class,
+            ConnectionError::class,
+        ]);
+    }
+
+    /**
+     * @return Subscription
+     */
+    public function subscribe(): Subscription
+    {
+        return $this->createSubscription("db-conn-event-" .
+            count($this->subscribers()) . "-" . substr(uniqid(), 0, 4));
+    }
+
+    /**
+     * @param ConnectionStateContext $context
+     * @return DispatchReport
+     */
+    public function dispatch(ConnectionStateContext $context): DispatchReport
+    {
+        return $this->dispatchEvent($context);
     }
 }
